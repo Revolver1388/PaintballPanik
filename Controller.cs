ce92a2b8ef6e3bcecd923148ff44181987087e81
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-05-18 21:46:08 -0400

    here

---
+++ Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    GameObject player;
+    private string Horizontal;
+    private string Vertical;
+    private string Shoot;
+    private string Accept;
+    private string Cancel;
+    int controllerNumber;
+
+    float horizontal;
+    float vertical;
+    string horizontalAxis;
+    string verticalAxis;
+    string aButton;
+    string xButton;
+    string rightTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    internal void setControllerNumber(int number)
+    {
+        controllerNumber = number;
+        horizontalAxis = "P" + controllerNumber + "Horizontal";
+        verticalAxis = "p" + controllerNumber + "Vertical";
+        aButton = "P" + controllerNumber + "Accept";
+        xButton = "P" + controllerNumber + "Cancel";
+        rightTrigger = "P" + controllerNumber + "Shoot";
+        player = GameObject.FindGameObjectWithTag("P" + controllerNumber);
+        player.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis(horizontalAxis);
+        vertical = Input.GetAxis(verticalAxis);
+    }
+}
---
+++ PlayerSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    int numberOfPlayers;
+    int maxPlayers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfPlayers = 0;
+        maxPlayers = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numberOfPlayers < maxPlayers)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Controller>().setControllerNumber(numberOfPlayers+1);
+            }
+        }
+    }
+}
+
